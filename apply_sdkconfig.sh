47a884d671cdad9195ad1cda4c9bf6759ad044ab
Szybet <53944559+Szybet@users.noreply.github.com>
2024-06-24 21:41:49 +0200

    sdkconfig from main repo

--- apply_sdkconfig.sh
+++ apply_sdkconfig.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cp sdkconfig-esp32 app/esp32-arduino-lib-builder/configs/defconfig.common
+if [ ! -f defconfig.common ]; then
+    cp app/esp32-arduino-lib-builder/configs/defconfig.common ./
+else
+    cp defconfig.common app/esp32-arduino-lib-builder/configs/defconfig.common
+fi
+
+curl https://raw.githubusercontent.com/Szybet/InkWatchy/master/resources/tools/buildTime/sdkconfig.defaults >> app/esp32-arduino-lib-builder/configs/defconfig.common
+
+sed -i '/CONFIG_PARTITION_TABLE_CUSTOM/d' app/esp32-arduino-lib-builder/configs/defconfig.common
+sed -i '/CONFIG_PARTITION_TABLE_CUSTOM_FILENAME/d' app/esp32-arduino-lib-builder/configs/defconfig.common
+sed -i '/CONFIG_PARTITION_TABLE_FILENAME/d' app/esp32-arduino-lib-builder/configs/defconfig.common
